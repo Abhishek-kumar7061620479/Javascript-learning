2f9d6fc8dde483f8e35d002f04850afec4999b2b
Abhishek-kumar7061620479 <95942551+Abhishek-kumar7061620479@users.noreply.github.com>
2026-05-02 13:30:56 +0000

    confirm

--- .devcontainer/basics/dataTypes.js
+++ .devcontainer/basics/dataTypes.js
@@ -6,12 +6,12 @@ let employee
  emp_project = null
 //  emp_Manager ; // this gives error because we cannot set the value for this.
 
- console.table([(emp_Name) , (emp_Id) , (emp_Status) , (emp_project)]);
+//  console.table([(emp_Name) , (emp_Id) , (emp_Status) , (emp_project)]);
 
- console.log(typeof(emp_Name));
- console.log(typeof(emp_Id));
- console.log(typeof(emp_Status));
- console.log(typeof(emp_project));
+//  console.log(typeof(emp_Name));
+//  console.log(typeof(emp_Id));
+//  console.log(typeof(emp_Status));
+//  console.log(typeof(emp_project));
  // console.log(typeof(emp_Manager));
 
  let score = "33abc"
@@ -28,7 +28,56 @@ let employee
 
  let valueNumber = Number(score);
 
- console.log(valueNumber); // gives output NaN 
+ //console.log(valueNumber); // gives output NaN 
+
+ // oprations
+   
+
+ // numbers
+//  const num1 = 2;
+//  const num2 = 3
+
+ let num1 = 2
+ let num2 = 4
+
+ let num3 = num1 + num2 
+
+ console.log(num3);
+
+ // string
+
+ let str1 = "Hello"
+ let str2 = "Abhishek"
+
+ let concatString = str1 + " " + str2 
+
+ console.log(concatString);
+
+// boolean
+
+let isLogged = true
+
+let isNotLogged = false
+
+console.log(+isLogged); // gives output 1 
+console.log(isLogged);  // gives output true
+
+
+// comparision
+
+console.log(null >= 0);
+console.log(null <= 0);
+console.log(null == 0); 
+console.log(null === 0);  // strict check - compare data types!!
+
+
+
+
+
+
+
+ 
+ 
 
  
  
